950e07cc01179310ae9f4fa0ca7f58bfb40ce11c
linroid <linroid@gmail.com>
2015-04-13 15:38:38 +0800

    Java heap OOM test

--- Java/src/com/linroid/java/jvm/HeapOOM.java
+++ Java/src/com/linroid/java/jvm/HeapOOM.java
@@ -1,9 +1,26 @@
 package com.linroid.java.jvm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * VM option: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
  * Created by linroid on 4/13/15.
  */
 public class HeapOOM {
+    static class OOMObject {
+
+    }
     public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        try {
+            while (true) {
+                list.add(new OOMObject());
+            }
+        } catch (OutOfMemoryError error){
+            error.printStackTrace();
+            System.err.println("共实例化了: " + list.size() + " 个OOMObject对象");
+        }
+
     }
 }

4dca6fa06f49992ccd2e6d9486e2ee189211483d
张林 <linroid@gmail.com>
2015-06-23 10:33:59 +0800

    初始化 《iOS编程》 第4章"Hypnosister"项目
    	new file:   Hypnosister/LRDHypnosisView.h
    	new file:   Hypnosister/LRDHypnosisView.m

---
+++ iOS/Hypnosister/Hypnosister/LRDHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  LRDHypnosisView.m
+//  Hypnosister
+//
+//  Created by 张林 on 15/6/23.
+//  Copyright (c) 2015年 张林. All rights reserved.
+//
+
+#import "LRDHypnosisView.h"
+
+@implementation LRDHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

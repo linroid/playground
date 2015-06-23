f395a654817a4fed4923f79d859bbb9fbb6c66b4
张林 <linroid@gmail.com>
2015-06-23 11:34:50 +0800

    绘制同心圆

--- iOS/Hypnosister/Hypnosister/LRDHypnosisView.m
+++ iOS/Hypnosister/Hypnosister/LRDHypnosisView.m
@@ -16,13 +16,19 @@
     center.x = (bounds.origin.x + bounds.size.width) / 2;
     center.y = (bounds.origin.y + bounds.size.height) / 2;
     
-    float radius = MIN(bounds.size.width, bounds.size.height) / 3;
+    float maxRadius = hypot(bounds.size.width, bounds.size.height);
+    
     UIBezierPath *path = [[UIBezierPath alloc] init];
-    [path addArcWithCenter:center
-                    radius:radius
-                startAngle:0.0
-                  endAngle:M_PI * 2.0
-                 clockwise:YES];
+    
+    for (int currentRadius=maxRadius; currentRadius>0; currentRadius-=20) {
+        [path moveToPoint:CGPointMake(center.x+currentRadius, center.y)];
+        [path addArcWithCenter:center
+                        radius:currentRadius
+                    startAngle:0.0
+                      endAngle:M_PI * 2.0
+                     clockwise:YES];
+
+    }
     
     path.lineWidth = 10;
     [[UIColor lightGrayColor] setStroke];

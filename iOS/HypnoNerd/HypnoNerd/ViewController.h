32349e08cea56e145980f42d5ace3541e5423051
张林 <linroid@gmail.com>
2015-06-23 14:30:54 +0800

    Initial Commit

---
+++ iOS/HypnoNerd/HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by 张林 on 15/6/23.
+//  Copyright (c) 2015年 张林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/HypnoNerd/HypnoNerd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HypnoNerd
+//
+//  Created by 张林 on 15/6/23.
+//  Copyright (c) 2015年 张林. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

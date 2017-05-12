369dc58de3cd688b52c4e45fdf6d51c875944917
张林 <zhanglin2@yy.com>
2017-05-12 18:35:54 +0800

    OpenGL: 添加混合着色

--- OpenGL/AirHockey/app/src/main/java/com/linroid/airhockey/GLRender.kt
+++ OpenGL/AirHockey/app/src/main/java/com/linroid/airhockey/GLRender.kt
@@ -1,16 +1,13 @@
 package com.linroid.airhockey
 
 import android.opengl.GLES20.*
-import android.opengl.GLUtils.*
-import android.opengl.Matrix.*
 import android.opengl.GLSurfaceView
 import com.linroid.airhockey.utils.GLLoader
 import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 import android.opengl.GLES20.glUniform4f
-
-
+import com.linroid.airhockey.utils.Sizeof
 
 
 /**
@@ -21,27 +18,28 @@ import android.opengl.GLES20.glUniform4f
 class GLRender(val surfaceView: GLSurfaceView) : GLSurfaceView.Renderer {
     private var fragShader: Int = 0
     private var vertexShader: Int = 0
-    private var colorLocation: Int = 0
-    private var positionLocation: Int = 0
+    private var aPositionLocation: Int = 0
+    private var aColorLocation: Int = 0
     private lateinit var vertexData: FloatBuffer
+    private val POINT_COMPONENT_COUNT  = 2
+    private val COLOR_COMPONENT_COUNT = 3
+    private val STRIDE = (POINT_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * Sizeof.FLOAT
     private val VERTEX_DATA_ARRAY = floatArrayOf(
-            // Triangle 1
-            -0.5f, -0.5f,
-            0.5f, 0.5f,
-            -0.5f, 0.5f,
-
-            // Triangle 2
-            -0.5f, -0.5f,
-            0.5f, -0.5f,
-            0.5f, 0.5f,
+            // Triangle
+            0f,     0f,     1f,     1f,     1f,
+            -0.5f,  -0.5f,  0.7f,   0.7f,   0.7f,
+            0.5f,  -0.5f,   0.7f,   0.7f,   0.7f,
+            0.5f,   0.5f,   0.7f,   0.7f,   0.7f,
+            -0.5f,  0.5f,   0.7f,   0.7f,   0.7f,
+            -0.5f,  -0.5f,  0.7f,   0.7f,   0.7f,
 
             // Line 1
-            -0.5f, 0f,
-            0.5f, 0f,
+            -0.5f,  0f,     1f,     0f,     0f,
+            0.5f,   0f,     1f,     0f,     0f,
 
             // Mallets
-            0f, -0.25f,
-            0f, 0.25f)
+            0f,     -0.25f, 0f,     0f,     1f,
+            0f,     0.25f,  1f,     0f,     0f)
 
     init {
         surfaceView.setEGLContextClientVersion(2)
@@ -57,13 +55,17 @@ class GLRender(val surfaceView: GLSurfaceView) : GLSurfaceView.Renderer {
         val program = GLLoader.link(vertexShader, fragShader)
         glUseProgram(program)
 
-        colorLocation = glGetUniformLocation(program, "u_Color")
-        positionLocation = glGetAttribLocation(program, "a_Position")
+        aPositionLocation = glGetAttribLocation(program, "a_Position")
+        aColorLocation = glGetAttribLocation(program, "a_Color")
 
         vertexData.position(0)
-        glVertexAttribPointer(positionLocation, 2, GL_FLOAT, false, 0, vertexData)
+        glVertexAttribPointer(aPositionLocation, POINT_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData)
 
-        glEnableVertexAttribArray(positionLocation)
+        vertexData.position(POINT_COMPONENT_COUNT)
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData )
+
+        glEnableVertexAttribArray(aPositionLocation)
+        glEnableVertexAttribArray(aColorLocation)
     }
 
     override fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {
@@ -74,19 +76,15 @@ class GLRender(val surfaceView: GLSurfaceView) : GLSurfaceView.Renderer {
         glClear(GL_COLOR_BUFFER_BIT)
 
         // draw two triangles
-        glUniform4f(colorLocation, 1F, 1F, 1F, 1F)
-        glDrawArrays(GL_TRIANGLES, 0, 6)
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6)
 
         // draw lines
-        glUniform4f(colorLocation, 1F, 0F, 0F, 1F)
         glDrawArrays(GL_LINES, 6, 2)
 
         // draw first mallet
-        glUniform4f(colorLocation, 0F, 0F, 1F, 1F)
         glDrawArrays(GL_POINTS, 8, 1)
 
         // draw second mallet
-        glUniform4f(colorLocation, 1F, 0F, 0F, 1F)
         glDrawArrays(GL_POINTS, 9, 1)
     }
 

dcfb2b74d3b30d0e0421533a835109bef110a663
Lin Zhang <linroid@gmail.com>
2022-10-09 23:36:36 +0800

    [LeetCode]94. Binary Tree Inorder Traversal

---
+++ leetcode/binary_tree/94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+        return result;
+    }
+    
+    void inorder(TreeNode* node, vector<int> &result) {
+        if (node == nullptr) {
+            return;
+        }
+        inorder(node->left, result);
+        result.push_back(node->val);
+        inorder(node->right, result);
+    }
+};
